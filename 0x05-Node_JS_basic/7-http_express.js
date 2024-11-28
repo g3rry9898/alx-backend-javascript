cd881ccefbef1e7b6a3a2de33bbd2c027e4c9589
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 09:35:02 -0800

    l

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const databasePath = path.resolve(__dirname, process.argv[2]);
+  countStudents(databasePath)
+    .then(() => {
+      res.end();
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
+
