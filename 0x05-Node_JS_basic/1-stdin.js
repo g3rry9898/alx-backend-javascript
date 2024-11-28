485b4fbad4beed303c5a5c1824b661137af2e4f7
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 08:58:13 -0800

    l

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-// 1-stdin.js
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+});
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
 
