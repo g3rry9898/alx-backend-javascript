4ffb6af6bc6319ca821c713de01598bb72c14b53
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 10:08:34 -0800

    l

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 when inputs are 1.4 and 4.5', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+
+    it('should return 5 when inputs are 1.2 and 3.7', () => {
+      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return -4 when inputs are 1.4 and 4.5', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+
+    it('should return -3 when inputs are 1.2 and 3.7', () => {
+      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when dividing by 0', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
+
