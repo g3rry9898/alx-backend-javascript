e904ae9ae849c0537f8820f24eba054abd76f07d
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 10:01:11 -0800

    l

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+
+    it('should return 5 when inputs are 1.2 and 3.7', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return -4 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+
+    it('should return -3 when inputs are 1.2 and 3.7', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" when dividing by 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
+
