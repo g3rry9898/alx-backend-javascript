34aa7eeafcf21c7904aac1663dc3d52f0de76815
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 09:06:19 -0800

    l

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-    const students = lines.slice(1).map(line => line.split(','));
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(','));
 
     const numberOfStudents = students.length;
     console.log(`Number of students: ${numberOfStudents}`);
 
     const fields = {};
-    students.forEach(student => {
+    students.forEach((student) => {
       const field = student[3];
       const firstName = student[0];
       if (!fields[field]) {
@@ -20,9 +20,9 @@ function countStudents(path) {
       fields[field].push(firstName);
     });
 
-    for (const [field, names] of Object.entries(fields)) {
+    Object.entries(fields).forEach(([field, names]) => {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-    }
+    });
   } catch (error) {
     throw new Error('Cannot load the database');
   }
