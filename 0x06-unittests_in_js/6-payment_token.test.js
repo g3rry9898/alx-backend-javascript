bfba74959a52c9d1e76b1a7acee231b4045411d0
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 10:25:34 -0800

    l

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with the correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
+
