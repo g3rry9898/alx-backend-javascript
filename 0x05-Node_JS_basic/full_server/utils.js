3b37ba7d2066522d58f6306c2e2fbc79b29c91a3
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 09:49:08 -0800

    l

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+export function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const students = lines.slice(1).map(line => line.split(','));
+      const fields = {};
+
+      students.forEach(student => {
+        const field = student[3];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(student[0]);
+      });
+
+      resolve(fields);
+    });
+  });
+}
+
