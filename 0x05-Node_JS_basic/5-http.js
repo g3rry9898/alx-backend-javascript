32f1b6babdb1a8f873484f9720f7bb5a69a45658
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 09:26:14 -0800

    l

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,8 +1,29 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
+    countStudents(path.resolve(__dirname, 'database.csv'))
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
 app.listen(1245, () => {
