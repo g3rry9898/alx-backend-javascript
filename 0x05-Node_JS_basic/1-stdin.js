814399b79e4b624d9a4fe87b3c270fb555898b5a
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-28 09:01:02 -0800

    l

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-
+// 1-stdin.js
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
@@ -9,4 +9,7 @@ process.stdin.on('data', (data) => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
+
+// To handle EOF when piping input
+process.stdin.resume();
 
